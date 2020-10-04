1cf372951a15b9dd60b7ae50e6ba8512d5dcb6eb
TTakeshik <71369975+TTakeshik@users.noreply.github.com>
2020-10-04 12:07:15 +0300

    Add files via upload

---
+++ Laba#4 - 5(main).cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#define N 100
+
+void main(void)
+{
+	setlocale(LC_ALL, "Russian");
+	int mas[N];
+	int n, i;
+	const int kol = 5;
+	printf("Введите кол-во элементов в массиве: ", N - 1);
+	scanf_s("%d", &n);
+	printf("Заполните массив:");
+	for (i = 0; i < n; i++)
+		scanf_s("%d", &mas[i]);
+	int min = mas[0];
+	for (int i = 0; i < n; i++)
+	{
+		for (int x = 0; x < n; x++)
+		{
+			if (mas[x] < mas[min])
+				min = x;
+			for (int z = min; z < n; z++)
+				mas[z] = mas[z + 1];
+		}
+	}
+	for (int i = 0; i < n - kol; i++)
+		printf("%d", mas[i]);
+
+}
